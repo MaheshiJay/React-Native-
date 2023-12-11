93408b7df0ff111d1dc30745676f1928fc0012eb
Maheshi <maheshirj4@gmail.com>
2023-12-11 19:03:37 +0530

    Grid list added

---
+++ TheApp/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import CategoriesScreen from './screens/CategoriesScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+     <CategoriesScreen/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+  },
+});
